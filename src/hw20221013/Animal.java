310dc48f527e05da938a36f6b0f64759c122f0b1
oleksiibabaikin <a.babaykin@gmail.com>
2022-10-18 18:00:44 +0300

    second HomeWork 13.10.2022

---
+++ src/hw20221013/Animal.java
@@ -0,0 +1,6 @@
+package hw20221013;
+
+public interface Animal {
+    void eat(int time);
+    void voice();
+}
---
+++ src/hw20221013/Dog.java
@@ -0,0 +1,13 @@
+package hw20221013;
+
+public class Dog extends AnimalType{
+
+    public Dog(String type, String name) {
+        super(type, name);
+    }
+
+    @Override
+    public void voice() {
+        System.out.println("My voice is Gaw-Gaw");
+    }
+}
